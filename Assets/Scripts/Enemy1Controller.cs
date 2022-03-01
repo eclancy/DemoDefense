031ca536c5a0039c09ed03148d73e18691ddcc60
Jasonbluefire <Jasonbluefire@gmail.com>
2022-03-01 00:37:51 -0500

    Movement By Force
    Movement By Force

--- Assets/Scripts/Enemy1Controller.cs
+++ Assets/Scripts/Enemy1Controller.cs
@@ -6,43 +6,45 @@ public class Enemy1Controller : MonoBehaviour
 {
     private Rigidbody2D EnemyBody;
     private string CurrentDirection;
+    private int PathStep = 0;
     private int MovementSpeed = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         EnemyBody = gameObject.GetComponent<Rigidbody2D>();
-        //Vector3 TargetPosition = new Vector3(-4.5f,-0.5f,0);
 
-        //EnemyBody.MovePosition(transform.position + TargetPosition * MovementSpeed * Time.deltaTime);
-        //Move("Right");
+        Move("Right");
     }
 
     // Update is called once per frame
     void Update()
     {
         double XLocation = EnemyBody.position.x;
-        
-        //Vector3 TargetPosition = new Vector3(-4.5f,-0.5f,0);
+        double YLocation = EnemyBody.position.y;
 
-        Vector3 TargetPosition = new Vector3(1f,0,0);
+        if(XLocation > -4.45 && PathStep == 0)
+        { Move("Up"); PathStep = 1; }
+        else if(YLocation > 2.5 && PathStep == 1)
+        { Move("Right"); PathStep = 2; }
+        else if(XLocation > -1.5 && PathStep == 2)
+        { Move("Down"); PathStep = 3; }
+        else if (YLocation > -1.5 && PathStep == 3)
+        { Move("Right"); PathStep = 4; }
 
-        EnemyBody.MovePosition((transform.position + TargetPosition) * 1 * Time.deltaTime);
 
-       // if(XLocation > 0 && CurrentDirection != "Up")
-       // { Move("Up"); }
-
-        int a = 1;
-        //Move();
     }
 
     //Path 3 right, 3 up, 3 right, 4 down, 4 right, 2 up, 6 right
     // -4.5, -0.5
+    // -4.5, 2.5
+    // -1.5, 2.5
+    // -1.5, -1.5
     void Move(string Direction)
     {
         CurrentDirection = Direction;
-        EnemyBody.MovePosition(new Vector3(-4.5f,-0.5f,0));
-        if(Direction == "Right")
+        EnemyBody.velocity = new Vector2(0, 0);
+        if (Direction == "Right")
         { EnemyBody.AddForce(new Vector2(MovementSpeed, 0), ForceMode2D.Impulse); }
         if(Direction == "Left")
         { EnemyBody.AddForce(new Vector2((MovementSpeed * -1), 0), ForceMode2D.Impulse); }

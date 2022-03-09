9eabb8601c82147235028bbfcc7fcd84b8242852
Eric Clancy <eric.clancy@maine.edu>
2022-03-08 23:45:32 -0500

    updated pathing to work with left, right, up, down

--- Assets/Scripts/Enemy1Controller.cs
+++ Assets/Scripts/Enemy1Controller.cs
@@ -10,7 +10,7 @@ public class Enemy1Controller : MonoBehaviour
 
     private int PathStep = 1;
 
-    private float MovementSpeed = 1;
+    private float MovementSpeed = 10;
 
     private double MaxHealth = 100;
 
@@ -28,26 +28,74 @@ public class Enemy1Controller : MonoBehaviour
         targetWaypoint = waypoints[PathStep];
         Move();
 
-        Debug.Log(targetWaypoint);
-
+        Debug.Log (targetWaypoint);
     }
 
     void Update()
     {
-
-        if (Vector3.Distance(transform.position, targetWaypoint.transform.position) < 0.1f)
-        {
+        if (Vector3.Distance(transform.position, targetWaypoint.transform.position) < 0.1f ){
             PathStep++;
             targetWaypoint = waypoints[PathStep];
-            Debug.Log(targetWaypoint);
-            Move ();
+            Debug.Log (targetWaypoint);
+            Move();
         }
     }
 
     void Move()
     {
+        //reset the velocity
         rigidBody.velocity = new Vector2(0, 0);
-        rigidBody.AddForce( (targetWaypoint.transform.position - transform.position) * MovementSpeed, ForceMode2D.Impulse );
+
+        // rigidBody.AddForce( (targetWaypoint.transform.position - transform.position) * MovementSpeed, ForceMode2D.Impulse );
+        string Direction;
+        float xDistance =
+            targetWaypoint.transform.position.x - transform.position.x;
+        float yDistance =
+            targetWaypoint.transform.position.y - transform.position.y;
+
+        //x distance is greater, figure out if we should move left or right
+        if (Mathf.Abs(xDistance) > Mathf.Abs(yDistance))
+        {
+            if (xDistance > 0){
+                Direction = "Right";
+            }
+            else{
+                Direction = "Left";
+            }
+        }
+        else
+        //y distance is greater, figure out if we should move up or down
+        {
+            if (yDistance > 0) {
+                Direction = "Up";
+            }
+            else {
+                Direction = "Down";
+            }
+        }
+
+        if (Direction == "Right")
+        {
+            rigidBody
+                .AddForce(new Vector2(MovementSpeed, 0), ForceMode2D.Impulse);
+        }
+        if (Direction == "Left")
+        {
+            rigidBody
+                .AddForce(new Vector2((MovementSpeed * -1), 0),
+                ForceMode2D.Impulse);
+        }
+        if (Direction == "Up")
+        {
+            rigidBody
+                .AddForce(new Vector2(0, MovementSpeed), ForceMode2D.Impulse);
+        }
+        if (Direction == "Down")
+        {
+            rigidBody
+                .AddForce(new Vector2(0, (MovementSpeed * -1)),
+                ForceMode2D.Impulse);
+        }
     }
 
     public void TakeDamage(string DamageType, double DamageAmount)

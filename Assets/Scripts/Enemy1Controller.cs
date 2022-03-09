b90e8c44160482466fe991ca8a73ca3905f6515f
Eric Clancy <eric.clancy@maine.edu>
2022-03-09 01:42:31 -0500

    fixed waypoint calculations

--- Assets/Scripts/Enemy1Controller.cs
+++ Assets/Scripts/Enemy1Controller.cs
@@ -25,18 +25,15 @@ public class Enemy1Controller : MonoBehaviour
         this.rigidBody = gameObject.GetComponent<Rigidbody2D>();
 
         waypoints = transform.parent.GetComponent<WaypointReference>().GetWaypoints();
-        targetWaypoint = waypoints[PathStep];
+        targetWaypoint = waypoints[PathStep]; 
         Move();
-
-        Debug.Log (targetWaypoint);
     }
 
     void Update()
     {
-        if (Vector3.Distance(transform.position, targetWaypoint.transform.position) < 0.1f ){
+        if ( Vector3.Distance(transform.position, targetWaypoint.transform.position) < 1.0f ) {
             PathStep++;
             targetWaypoint = waypoints[PathStep];
-            Debug.Log (targetWaypoint);
             Move();
         }
     }
@@ -46,78 +43,29 @@ public class Enemy1Controller : MonoBehaviour
         //reset the velocity
         rigidBody.velocity = new Vector2(0, 0);
 
-        // rigidBody.AddForce( (targetWaypoint.transform.position - transform.position) * MovementSpeed, ForceMode2D.Impulse );
-        string Direction;
-        float xDistance =
-            targetWaypoint.transform.position.x - transform.position.x;
-        float yDistance =
-            targetWaypoint.transform.position.y - transform.position.y;
+        //reassign which direction "right" is
+        transform.right = targetWaypoint.position - transform.position;
 
-        //x distance is greater, figure out if we should move left or right
-        if (Mathf.Abs(xDistance) > Mathf.Abs(yDistance))
-        {
-            if (xDistance > 0){
-                Direction = "Right";
-            }
-            else{
-                Direction = "Left";
-            }
-        }
-        else
-        //y distance is greater, figure out if we should move up or down
-        {
-            if (yDistance > 0) {
-                Direction = "Up";
-            }
-            else {
-                Direction = "Down";
-            }
-        }
+        //add force in the "right" direction, to move toward the next waypoint
+        rigidBody.AddForce(transform.right * 5, ForceMode2D.Impulse);
 
-        if (Direction == "Right")
-        {
-            rigidBody
-                .AddForce(new Vector2(MovementSpeed, 0), ForceMode2D.Impulse);
-        }
-        if (Direction == "Left")
-        {
-            rigidBody
-                .AddForce(new Vector2((MovementSpeed * -1), 0),
-                ForceMode2D.Impulse);
-        }
-        if (Direction == "Up")
-        {
-            rigidBody
-                .AddForce(new Vector2(0, MovementSpeed), ForceMode2D.Impulse);
-        }
-        if (Direction == "Down")
-        {
-            rigidBody
-                .AddForce(new Vector2(0, (MovementSpeed * -1)),
-                ForceMode2D.Impulse);
-        }
     }
 
     public void TakeDamage(string DamageType, double DamageAmount)
     {
-        if (DamageType == "Fire")
-        {
+        if (DamageType == "Fire") {
             CurrentHealth = CurrentHealth - (DamageAmount * 1.5);
         }
-        else
-        {
+        else {
             CurrentHealth = CurrentHealth - DamageAmount;
         }
 
-        if (CurrentHealth <= 0)
-        {
+        if (CurrentHealth <= 0) {
             Destroy (gameObject);
         }
-        else
-        {
+        else {
             Color tmp = gameObject.GetComponent<SpriteRenderer>().color;
-            float Transparency =
-                ((1 / (float) MaxHealth) * (float) CurrentHealth);
+            float Transparency = ((1 / (float) MaxHealth) * (float) CurrentHealth);
             tmp.a = Transparency;
             gameObject.GetComponent<SpriteRenderer>().color = tmp;
         }
